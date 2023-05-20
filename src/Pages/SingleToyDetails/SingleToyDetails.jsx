2badc968add897cca8d4b19b29446bb7b6e93229
75mdnazmul <75mdnazmulhasan@gmail.com>
2023-05-20 11:07:26 +0600

    Single Toy Details page is added

---
+++ src/Pages/SingleToyDetails/SingleToyDetails.jsx
@@ -0,0 +1,33 @@
+import { useLoaderData } from "react-router-dom";
+
+
+const SingleToyDetails = () => {
+    const singleToyDetails = useLoaderData()
+    const { id, musicToyName, picture, sellerName, sellerEmail, price, rating, availableQuantity, detailDescription } = singleToyDetails;
+    return (
+        <>
+            <h2 className="container mt-5 text-center fw-bold">{musicToyName}</h2>
+            <div className="card mb-3 container my-5 p-5">
+                <div className="row g-0" key={id}>
+                    <div className="col-md-4">
+                        <img src={picture} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title"><span className="fw-bold">Toy Name : </span>{musicToyName}</h5>
+                            <p className="card-text"><span className="fw-bold">Detail Description : </span>{detailDescription}</p>
+                            <p className="card-text"><span className="fw-bold">Seller Name : </span> {sellerName}</p>
+                            <p className="card-text"><span className="fw-bold">Seller Email : </span>{sellerEmail}</p>
+                            <p className="card-text"><span className="fw-bold">Price : </span>{price}$</p>
+                            <p className="card-text"><span className="fw-bold">Rating: </span>{rating}</p>
+                            <p className="card-text"><span className="fw-bold">Available Quantity : </span>{availableQuantity}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+};
+
+export default SingleToyDetails;
